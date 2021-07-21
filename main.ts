4df5f891ae547b4fe1cfc1b2de5184117b7e0e21
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-07-21 17:14:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    if (Move == 0) {
+        Move = 1
+        for (let index = 0; index < randint(5, 20); index++) {
+            Fireball.move(1)
+            Fireball.ifOnEdgeBounce()
+            basic.pause(100)
+        }
+        Fireball.set(LedSpriteProperty.X, 1)
+        Fireball.set(LedSpriteProperty.Y, 3)
+        Move = 0
+    }
+})
+let Move = 0
+let Fireball: game.LedSprite = null
+game.setLife(5)
+let Monster = game.createSprite(4, 0)
 let Wizard = game.createSprite(1, 3)
-let Fireball = game.createSprite(1, 3)
+Fireball = game.createSprite(1, 3)
+Move = 0
 basic.forever(function () {
-	
+    if (Move == 0) {
+        Fireball.turn(Direction.Right, randint(0, 359))
+        if (Fireball.isTouching(Monster)) {
+            game.addScore(25)
+        }
+    }
+    basic.pause(500)
+})
+basic.forever(function () {
+    Monster.turn(Direction.Right, randint(0, 359))
+    Monster.move(0.5)
+    Monster.ifOnEdgeBounce()
+    if (Wizard.isTouching(Monster)) {
+        game.removeLife(1)
+        Monster.set(LedSpriteProperty.X, 4)
+        Monster.set(LedSpriteProperty.Y, 0)
+    }
+    basic.pause(750)
 })
